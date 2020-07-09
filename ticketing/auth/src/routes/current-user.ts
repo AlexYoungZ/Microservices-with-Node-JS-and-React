e4cf4c4aa9999add15d0b3127b2b2664e1bd3b12
AlexYoungZ <zhangsiy2015@163.com>
2020-07-09 16:30:14 +0800

    update auth

---
+++ ticketing/auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { currentUser } from '@tickets/common';
+
+const router = express.Router();
+
+router.get('/api/users/currentUser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
+
+export { router as currentUserRouter };
